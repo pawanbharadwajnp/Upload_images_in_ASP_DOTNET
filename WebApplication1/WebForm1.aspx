a40f63f2f51d01bf6ec0f95c8c7b4a928dbed077
nppavan bharadwaj <pawanbharadwajnp@gmail.com>
2022-09-24 16:54:51 +0530

    Add project files.

---
+++ WebApplication1/WebForm1.aspx
@@ -0,0 +1,74 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 100%;
+        }
+        .auto-style2 {
+            width: 362px;
+        }
+        .auto-style3 {
+            width: 362px;
+            height: 34px;
+        }
+        .auto-style4 {
+            height: 34px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <br />
+        <table cellpadding="4" cellspacing="4" class="auto-style1">
+            <tr>
+                <td class="auto-style2">Image Upload</td>
+                <td>
+                    <asp:FileUpload ID="FileUpload1" runat="server" />
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style3"></td>
+                <td class="auto-style4">
+                    <asp:Button ID="Button1" runat="server" OnClick="Button1_Click1" Text="Save" Width="105px" />
+&nbsp;&nbsp;
+                    <asp:Label ID="Label1" runat="server" Text="Label" Visible="False"></asp:Label>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">&nbsp;</td>
+                <td>
+                <%--     to do this select grid view ->properties->behaviour->autoGenerateColumns->false--%>
+                    <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" ShowHeader="False">
+                        <Columns>
+                            <asp:TemplateField> 
+                                <ItemTemplate> 
+                                    <table>
+
+                                        <tr>  
+                                            <td>
+                                                <asp:Label ID="Label2" runat="server" Text='<%# Eval("id") %> '></asp:Label> <!--gets id from database-->
+                                            </td>
+                                            <td>
+                                                <asp:Image Height="150" Width="150" ID="Image1" runat="server" ImageUrl='<%# Eval("image_name") %> ' />
+
+                                            </td>
+                                        </tr>
+
+                                    </table>
+
+                                </ItemTemplate>
+                            </asp:TemplateField>
+
+                        </Columns>
+                    </asp:GridView>
+                </td>
+            </tr>
+        </table>
+    </form>
+    </body>
+</html>
---
+++ WebApplication1/WebForm1.aspx.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+// import all sql librabries
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+using System.IO;
+//------------------------------
+
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        string cs = ConfigurationManager.ConnectionStrings["My_Database_String_in_AppConfig_file"].ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack) //only when page o-eneing 1st time
+            {
+                GridView();
+            }
+            if (IsPostBack) // after upload is clicked
+            {
+                GridView();
+            }
+           
+        }
+
+        void GridView() // put gridview in web page and put id as GridView1
+        {
+            SqlConnection con = new SqlConnection(cs);
+            string querry = "select * from img";
+
+            SqlDataAdapter sda = new SqlDataAdapter(querry, con);
+
+            DataTable data = new DataTable();
+
+            sda.Fill(data);
+
+            GridView1.DataSource = data;
+            GridView1.DataBind();
+
+
+        }
+
+        protected void Button1_Click1(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(cs);
+
+            string path = Server.MapPath("images/"); //stores  images inside images folder
+
+            if ( FileUpload1.HasFile )
+            {
+                string fileName = Path.GetFileName( FileUpload1.FileName ); //gets path of the stored image
+
+                string extension = Path.GetExtension( fileName );
+
+                HttpPostedFile postedFile = FileUpload1.PostedFile; // stores file controls
+
+                int length = postedFile.ContentLength; //gets size of images in bytes
+
+
+                if(extension.ToLower() == ".jfif" || extension.ToLower() == " .jpg " || extension.ToLower() == ".png" || extension.ToLower() == ".jpeg")
+                {
+
+                    if (length <= 1000000000)
+                    {
+                        FileUpload1.SaveAs(path + fileName);
+
+                        string name = "images/" + fileName;
+
+                        //store in database
+                        string query = "insert into img values(@path) ";
+                        SqlCommand cmd = new SqlCommand(query, con);
+
+                        cmd.Parameters.AddWithValue("@path", name);
+
+                        con.Open();
+
+                        int a = cmd.ExecuteNonQuery();
+
+                        if (a > 0)
+                        {
+                            Label1.Text = "image uploaded sucessfully";
+                            Label1.ForeColor = System.Drawing.Color.Green;
+                            Label1.Visible = true;
+                            GridView();
+                        }
+                        else
+                        {
+                            Label1.Text = "image not uploaded";
+                            Label1.ForeColor = System.Drawing.Color.Red;
+                            Label1.Visible = true;
+                        }
+
+                        con.Close();
+
+                    }
+                    else
+                    {
+                        Label1.Text = "image size too big";
+                        Label1.ForeColor = System.Drawing.Color.Red;
+                        Label1.Visible = true;
+                    }
+
+
+                }
+                else
+                {
+                    Label1.Text = "image format not supported";
+                    Label1.ForeColor = System.Drawing.Color.Red;
+                    Label1.Visible = true;
+                }
+
+
+            }
+            else
+            {
+                Label1.Text = "select image";
+                Label1.ForeColor = System.Drawing.Color.Red;
+                Label1.Visible = true;
+            }
+
+        }
+    }
+}
